73454b1c66bd38721aa948bdbb4a3460e7e98321
upvisor <admin@upvisor.cl>
2025-03-17 18:53:23 -0300

    actualization general

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Page () {
           <h1 className='text-2xl my-auto font-medium'>Inicio</h1>
         </div>
         <div className='flex flex-col gap-6 w-full max-w-[1280px] mx-auto mb-4'>
-          <h2 className='text-2xl font-medium'>¡Hola! Te damos la bienvenida al panel de control de tu sitio web</h2>
+          <h2 className='text-2xl font-medium'>¡Hola! Te damos la bienvenida al panel administrativo de tu sitio web</h2>
         </div>
       </div>
     </>
