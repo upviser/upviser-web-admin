8817ea02e815652cb8e34f758641f176861b19cb
upvisor <admin@upvisor.cl>
2025-08-14 15:27:52 -0400

    actualization general

--- components/layouts/Navbar.tsx
+++ components/layouts/Navbar.tsx
@@ -330,15 +330,16 @@ export const Navbar: React.FC<PropsWithChildren> = ({ children }) => {
                   }, 200)
                 }
               }} className={`${account.opacity} ${account.view} transition-opacity duration-200 fixed z-50 w-full h-full mt-[1px]`}>
-                <div onMouseEnter={() => setAccount({ ...account, mouse: true })} onMouseLeave={() => setAccount({ ...account, mouse: false })} className={`${account.opacity === 'opacity-1' ? 'scale-100' : 'scale-90'} transition-transform duration-200 border border-black/5 p-6 w-64 bg-white rounded-xl h-fit ml-auto dark:bg-neutral-800`} style={{ boxShadow: '0px 3px 10px 3px #11111108' }}>
-                <button onClick={async (e: any) => {
-                  e.preventDefault()
-                  setAccount({ ...account, view: 'flex', opacity: 'opacity-0' })
-                  setTimeout(() => {
-                    setAccount({ ...account, view: 'hidden', opacity: 'opacity-0' })
-                  }, 200)
-                  await signOut()
-                }}>Cerrar sesión</button>
+                <div onMouseEnter={() => setAccount({ ...account, mouse: true })} onMouseLeave={() => setAccount({ ...account, mouse: false })} className={`${account.opacity === 'opacity-1' ? 'scale-100' : 'scale-90'} transition-transform duration-200 flex flex-col gap-2 border border-black/5 p-6 w-64 bg-white rounded-xl h-fit ml-auto dark:bg-neutral-800`} style={{ boxShadow: '0px 3px 10px 3px #11111108' }}>
+                  <Link href={'/configuracion-usuario'}>Mis datos</Link>
+                  <button onClick={async (e: any) => {
+                    e.preventDefault()
+                    setAccount({ ...account, view: 'flex', opacity: 'opacity-0' })
+                    setTimeout(() => {
+                      setAccount({ ...account, view: 'hidden', opacity: 'opacity-0' })
+                    }, 200)
+                    await signOut()
+                  }}>Cerrar sesión</button>
                 </div>
               </div>
               <div className={`${notificationsView.view} ${notificationsView.opacity} transition-opacity duration-200 w-full absolute z-50 flex`} onClick={(e: any) => {
